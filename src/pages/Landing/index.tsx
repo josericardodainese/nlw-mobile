e97025625f86bd6505b2a1cacf1573768d04d6c8
Ze <josericardodainese@gmail.com>
2020-08-11 09:43:47 -0300

    Finalizado Navegação em Tabs

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -9,6 +11,17 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png'
 
 function Landing() {
+
+    const { navigate } = useNavigation();
+
+    function handleNavigationToGiveClassPage() {
+        navigate('GiveClasses')
+    }
+
+    function handleNavigationToStudyPages() {
+        navigate('Study')
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -20,17 +33,19 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity
+                <RectButton
+                    onPress={handleNavigationToStudyPages}
                     style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Estudar</Text>
-                </TouchableOpacity>
+                </RectButton>
 
-                <TouchableOpacity
+                <RectButton
+                    onPress={handleNavigationToGiveClassPage}
                     style={[styles.button, styles.buttonSecondary]}>
                     <Image source={giveClassesIcon} />
                     <Text style={styles.buttonText}>Dar Aula</Text>
-                </TouchableOpacity>
+                </RectButton>
             </View>
             <Text style={styles.totalConnections}>
                 Total de 245 conexões ja realizadas {' '}
